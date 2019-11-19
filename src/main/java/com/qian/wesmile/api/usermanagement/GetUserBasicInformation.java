9fa59140f39e18b579090b5dfa63c6812268abe9
wuhuaiqian <wuhuaiqian@xlvren.com>
2019-11-19 10:10:00 +0800

    sns user info

--- src/main/java/com/qian/wesmile/api/usermanagement/GetUserBasicInformation.java
+++ src/main/java/com/qian/wesmile/api/usermanagement/GetUserBasicInformation.java
@@ -3,6 +3,7 @@ package com.qian.wesmile.api.usermanagement;
 import com.qian.wesmile.annotation.ParamName;
 import com.qian.wesmile.annotation.RelativePath;
 import com.qian.wesmile.model.result.GetOpenId;
+import com.qian.wesmile.model.result.SnsUserInfo;
 import com.qian.wesmile.model.result.UserInfo;
 
 public interface GetUserBasicInformation {
@@ -16,10 +17,33 @@ public interface GetUserBasicInformation {
     @RelativePath("/cgi-bin/user/info")
     UserInfo userInfo(@ParamName("openid") String openid, @ParamName("lang") String lang);
 
+    /**
+     * 通过code换取网页授权access_token
+     *
+     * @param appid
+     * @param secret
+     * @param code
+     * @param grant_type use default vale authorization_code
+     * @return
+     */
     @RelativePath("/sns/oauth2/")
     GetOpenId oauth2(@ParamName("appid") String appid,
                      @ParamName("secret") String secret,
                      @ParamName("code") String code,
                      @ParamName("grant_type") String grant_type);
+
+
+    /**
+     * 拉取用户信息(需scope为 snsapi_userinfo)
+     *
+     * @param accessToken
+     * @param openid
+     * @param lang
+     * @return
+     */
+    @RelativePath("/sns/userinfo")
+    SnsUserInfo snsUserInfo(@ParamName("access_token") String accessToken,
+                            @ParamName("openid") String openid,
+                            @ParamName("lang") String lang);
 
 }
---
+++ src/main/java/com/qian/wesmile/model/result/SnsUserInfo.java
@@ -0,0 +1,101 @@
+package com.qian.wesmile.model.result;
+
+import java.util.List;
+
+public class SnsUserInfo {
+
+
+    /**
+     * openid :  OPENID
+     * nickname : NICKNAME
+     * sex : 1
+     * province : PROVINCE
+     * city : CITY
+     * country : COUNTRY
+     * headimgurl : http://thirdwx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/46
+     * privilege : ["PRIVILEGE1","PRIVILEGE2"]
+     * unionid : o6_bmasdasdsad6_2sgVt7hMZOPfL
+     */
+
+    private String openid;
+    private String nickname;
+    private String sex;
+    private String province;
+    private String city;
+    private String country;
+    private String headimgurl;
+    private String unionid;
+    private java.util.List<String> privilege;
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getHeadimgurl() {
+        return headimgurl;
+    }
+
+    public void setHeadimgurl(String headimgurl) {
+        this.headimgurl = headimgurl;
+    }
+
+    public String getUnionid() {
+        return unionid;
+    }
+
+    public void setUnionid(String unionid) {
+        this.unionid = unionid;
+    }
+
+    public List<String> getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(List<String> privilege) {
+        this.privilege = privilege;
+    }
+}
